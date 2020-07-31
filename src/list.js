cd7886a7c77784b2fd4b3b6454e8e55877a7a871
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-07-31 16:59:34 +0100

    list component init

---
+++ src/list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = (props) => {
+  return (
+    <div>
+      hello
+    </div>
+  );
+};
+
+export default List;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import List from './list';
 
 const Main = (props) => {
 
    return (
       <div>
         hello
+        <List />
       </div>
    )
 };
