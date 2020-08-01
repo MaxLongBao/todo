033e061f9af15f4e32ec73a0253e7921e5d803be
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-08-01 02:02:00 +0100

    on change func

--- src/form.js
+++ src/form.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = (props) => {
+  const { handleSubmit } = props;
+
+  const [value, setValue] = useState('');
+  console.log(value)
+
   return (
     <div>
-      form
+      <form onSubmit={handleSubmit(value)} >
+        <label>
+          New Todo:
+          <input type='text' name='todo' onChange={e => setValue(e.target.value)} />
+        </label>
+      </form>
     </div>
   );
 };
--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 import List from './list';
 import Form from './form';
 
-const Main = (props) => {
+const Main = () => {
 
-  const [list, setList] = [];
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (value) => {
+    console.log(value);
+  };
 
    return (
       <div>
-        <Form />
+        <Form handleSubmit={handleSubmit}/>
         <List />
       </div>
-   )
+   );
 };
 
 export default Main;
